baf3a885214017618e0f53e4b7fc51e1c012c249
kazjon <kazjon@me.com>
2017-08-21 17:04:53 +1000

    Cleaned up Matthew's classifier code, added RecipeCategoriser.py

--- NBCSingle.py
+++ NBCSingle.py
@@ -9,32 +9,53 @@ from textblob.classifiers import NaiveBayesClassifier
 
 from textblob import TextBlob
 import csv
-import pickle
+import cPickle as pickle
 
 
 train = []
 test = []
 full = []
+classifiers = []
+for prefix in ["ind","chi","fre","gre","mex","ita","tha"]:
+	with open(prefix+'TrainData.csv', "rb") as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			train.append((row[0] + ": " + row[2],row[3]))
+
+	with open(prefix+'TestData.csv', "rb") as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			test.append((row[0] + ": " + row[2],row[3]))
+
+	print("Read data for " + prefix)
+
+	cl = NaiveBayesClassifier(train)
+	pickle.dump(cl,open(prefix+"Classifier.pkl","wb"))
+
+	#Compute accuracy
+	print "Model trained for " + prefix + ". Accuracy:" + str(cl.accuracy(test))
+
+	print "Most informative features for " + prefix + ":"
+	# Show 100 most informative features
+	cl.show_informative_features(50)
+	classifiers.append(cl)
+
+with open('notTaggedData.csv', "rb") as csvfile:
+	reader = csv.reader(csvfile)
+	for row in reader:
+		full.append((row[0] + ": " + row[2],row[3]))
+
+with open("CombinedOutput.csv","wb") as ofile:
+	writer = csv.writer(ofile)
+	for recipe in test:
+		print recipe,
+		classes = [recipe[0]]
+		for cl in classifiers:
+			classes.append(cl.prob_classify(recipe[0]).max())
+		print recipe
+		print "****    " + classes
+		writer.writerow(classes)
 """
-with open('indTrainData.csv', newline='', encoding="latin-1") as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        train.append((row[0] + ": " + row[2],row[3]))
-    
-with open('chiTestData.csv', newline='', encoding="latin-1") as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        test.append((row[0] + ": " + row[2],row[3]))
-"""
-with open('notTaggedData.csv', newline='', encoding="latin-1") as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        full.append((row[0] + ": " + row[2],row[3]))
-
-print("read data")
-
-#cl = NaiveBayesClassifier(train)
-
 cl1 = pickle.load(open("serialNBCChinese.pickle","rb"))#read PICKLE file of model
 cl2 = pickle.load(open("serialNBCFrench.pickle","rb"))
 cl3 = pickle.load(open("serialNBCGreek.pickle","rb"))
@@ -49,15 +70,15 @@ ofile = open('chiClassified.csv', "w", encoding="latin-1", newline='')
 writer = csv.writer(ofile)
 count = 0
 for recipe in full:
-    classes = [recipe[0]]
-    for cl in classifiers:
-        classes.append(cl.prob_classify(recipe[0]).max())
-    #print(classes)
-    writer.writerow(classes)
-    count+=1
-    if(count%100 == 0):
-        print(count)
-    
+	classes = [recipe[0]]
+	for cl in classifiers:
+		classes.append(cl.prob_classify(recipe[0]).max())
+	#print(classes)
+	writer.writerow(classes)
+	count+=1
+	if(count%100 == 0):
+		print(count)
+	
 ofile.close()
 # Compute accuracy
 #print("Accuracy: {0}".format(cl.accuracy(test)))
@@ -66,3 +87,4 @@ ofile.close()
 ##cl.show_informative_features(100)
 
 #pickle.dump(cl,open( "serialNBCIndian.pickle", "wb" ))
+"""
---
+++ RecipeCategoriser.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 14 10:16:24 2017
+
+@author: mzent
+"""
+
+from textblob.classifiers import NaiveBayesClassifier
+
+from textblob import TextBlob
+import csv
+import cPickle as pickle
+
+
+train = []
+test = []
+full = []
+classifiers = []
+for prefix in ["ind","chi","fre","gre","mex","ita","tha"]:
+	with open(prefix+'TrainData.csv', "rb") as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			train.append((row[0] + ": " + row[2],row[3]))
+
+	with open(prefix+'TestData.csv', "rb") as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			test.append((row[0] + ": " + row[2],row[3]))
+
+	print("Read data for " + prefix)
+
+	cl = NaiveBayesClassifier(train)
+	pickle.dump(cl,open(prefix+"Classifier.pkl","wb"))
+
+	#Compute accuracy
+	print "Model trained for " + prefix + ". Accuracy:" + str(cl.accuracy(test))
+
+	print "Most informative features for " + prefix + ":"
+	# Show 100 most informative features
+	cl.show_informative_features(50)
+	classifiers.append(cl)
+
+print "Trained all classifiers, loading untagged data."
+
+with open('notTaggedData.csv', "rb") as csvfile:
+	reader = csv.reader(csvfile)
+	for row in reader:
+		full.append((row[0] + ": " + row[2],row[3]))
+
+print "Data loaded, predicting classes for whole dataset."
+
+with open("CombinedOutput.csv","wb") as ofile:
+	writer = csv.writer(ofile)
+	for recipe in full:
+		print recipe,
+		classes = [recipe[0]]
+		for cl in classifiers:
+			classes.append(cl.prob_classify(recipe[0]).max())
+		print recipe
+		print "****    " + classes
+		writer.writerow(classes)
